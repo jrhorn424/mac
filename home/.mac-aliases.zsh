50ca428a06bd46ed06017db5307a96a314a0514a
Jeffrey Horn <jrhorn424@gmail.com>
2013-09-23 03:21:47 -0400

    Use IS_MAC to check

--- home/.mac-aliases.zsh
+++ home/.mac-aliases.zsh
@@ -1,2 +1,4 @@
-alias o='open . &'
-alias bup='brew update && brew outdated && brew doctor'
+if [[ $IS_MAC -eq 1 ]]; then
+  alias o='open . &'
+  alias bup='brew update && brew outdated && brew doctor'
+fi
